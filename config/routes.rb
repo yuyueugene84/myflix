1979af836bc7f6d20ce329948c048695548fe4d6
yuyueugene84 <yuyueugene84@gmail.com>
2016-01-07 00:45:51 +0800

    added search by title UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
+  root to: "videos#home"
   get 'home', to: 'videos#home'
-  resources 'videos'
+  resources 'videos' do
+    collection do
+      get 'search', to: 'videos#search'
+    end
+  end
 end
