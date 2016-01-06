e779732845e759099b08f4f361fe8192b9b05b77
yuyueugene84 <yuyueugene84@gmail.com>
2016-01-06 21:17:39 +0800

    implement serach_by_title method

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,9 @@
 class Video < ActiveRecord::Base
   belongs_to :category
   validates :title, :description, presence: true
+
+  def self.search_by_title(title)
+    return [] if title.blank?
+    Video.where("title LIKE ?", "%#{title}%").order(:created_at)
+  end
 end
