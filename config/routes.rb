f45efd4280349f180cb0edb43191902877083e8f
yuyueugene84 <yuyueugene84@gmail.com>
2016-01-14 21:30:53 +0800

    added front page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
-  root to: "videos#home"
+  root to: "pages#front"
   get 'home', to: 'videos#home'
   resources 'videos' do
     collection do
