8cdced4f17cc43dc0173678cd32d8830196ac345
yuyueugene84 <yuyueugene84@gmail.com>
2016-01-14 21:26:17 +0800

    added category spec

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -4,49 +4,59 @@ describe Category do
   it { should have_many(:videos)}
 
   describe "#recent_videos" do 
-    it 'should return the most recent six videos from this category' do 
+    it "should return an empty arrayif there is no video" do
       drama = Category.create(name: "Drama")
-      dramas = []
-      7.times do |v|
-        video = Video.create(title: "drama_#{v}", description: "test data", category_id: drama.id)
-        dramas << video
-      end
-      dramas.pop
-      expect(drama.recent_videos).to eq(dramas)
+      expect(drama.recent_videos).to eq([])
     end
 
-    it 'return all videos if there are less than six videos' do 
-      drama = Category.create(name: "Drama")
-      futurama = Video.create(
-        title: "Futurama",
-        description: "A famous cartoon about future",
-        category_id: drama.id)
+    it "should return videos that belong in the same category" do
       house_of_cards = Video.create(
         title: "House of Cards",
         description: "Story about Dirty Politician, Frank Underwood, and his rise to power.",
-        category_id: drama.id)
+        created_at: 1.day.ago)
+
       man_in_the_high_castle = Video.create(
         title: "Man in High Castle",
-        description: "Series based on novel about an alternate world where Axis power won WWII",
-        category_id: drama.id)
+        description: "Series based on novel about an alternate world where Axis power won WWII")
 
-      expect(drama.recent_videos).to eq([futurama, house_of_cards, man_in_the_high_castle])
+      drama = Category.create(name: "Drama")
+      drama.videos << man_in_the_high_castle
+      drama.videos << house_of_cards
+
+      expect(drama.recent_videos).to eq([man_in_the_high_castle, house_of_cards])
     end
 
-    it 'order of video should be reverse chronically ordered' do 
+    it "should return all videos if there are less than 6 videos" do
+      house_of_cards = Video.create(
+        title: "House of Cards",
+        description: "Story about Dirty Politician, Frank Underwood, and his rise to power.",
+        created_at: 1.day.ago)
+
+      man_in_the_high_castle = Video.create(
+        title: "Man in High Castle",
+        description: "Series based on novel about an alternate world where Axis power won WWII")
+
       drama = Category.create(name: "Drama")
-      dramas = []
-      6.times do |v|
-        video = Video.create(title: "drama_#{v}", description: "test data", category_id: drama.id, created_at: v.days.ago)
-        dramas << video
+      drama.videos << man_in_the_high_castle
+      drama.videos << house_of_cards
+
+      expect(drama.recent_videos).to eq([man_in_the_high_castle, house_of_cards])
+    end
+
+    it "should return the six videos if there are more than six videos" do
+      drama = Category.create(name: "Drama")
+
+      7.times do |v|
+        video = Video.create(
+        title: "test video#{v}",
+        description: "test data video#{v}",
+        created_at: v.day.ago)
+        drama.videos << video
       end
 
-      expect(drama.recent_videos).to eq(dramas.reverse!)
-    end
-
-    it 'should return empty array if there is no videos in the category' do 
-      comedy = Category.create(name: "comedy")
-      expect(comedy.recent_videos).to eq([])
+      expect(drama.recent_videos.length).to eq(6)
     end
   end
+
+
 end
