1cd6081844adeb97b69dba180f01141fe431bb7a
yuyueugene84 <yuyueugene84@gmail.com>
2016-01-21 20:31:43 +0800

    added sessions controller, front and login pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Myflix::Application.routes.draw do
-  get 'ui(/:action)', controller: 'ui'
   root to: "pages#front"
   get 'home', to: 'videos#home'
-  resources 'videos' do
+
+  get '/register', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
+  resources :videos do
     collection do
       get 'search', to: 'videos#search'
     end
   end
+
+  resources :users, only: [:new, :create]
+  get 'ui(/:action)', controller: 'ui'
 end
